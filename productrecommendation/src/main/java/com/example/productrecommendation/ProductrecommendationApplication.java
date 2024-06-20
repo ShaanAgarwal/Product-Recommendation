43121294127b34ce82a1a975b781cdbab840ee65
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-06-20 20:12:52 +0530

    Initial Project Files Downloaded

---
+++ productrecommendation/src/main/java/com/example/productrecommendation/ProductrecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.productrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductrecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductrecommendationApplication.class, args);
+	}
+
+}
